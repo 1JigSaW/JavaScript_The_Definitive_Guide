7732507e6dc225b59acb37b2f3fcb01595a17506
JigSaW <misho_1998@mail.ru>
2021-07-03 13:28:13 +0300

    math

---
+++ Chapter_16/chapter_16.js
@@ -0,0 +1,34 @@
+const x = 19.51;
+console.log(x.toFixed(3));
+console.log(x.toFixed(2));
+console.log(x.toFixed(1));
+console.log(x.toFixed(0));
+
+const x2 = 3800.5;
+console.log(x2.toExponential(4));
+console.log(x2.toExponential(3));
+console.log(x2.toExponential(2));
+console.log(x2.toExponential(1));
+console.log(x2.toExponential(0));
+
+let x3 = 1000;
+console.log(x3.toPrecision(5));
+console.log(x3.toPrecision(4));
+console.log(x3.toPrecision(3));
+console.log(x3.toPrecision(2));
+console.log(x3.toPrecision(1));
+
+const x4 = 12;
+console.log(x4.toString());
+console.log(x4.toString(10));
+console.log(x4.toString(16));
+console.log(x4.toString(8));
+console.log(x4.toString(2));
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+console.log(Math.SQRT2);
